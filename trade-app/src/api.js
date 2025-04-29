a0eb6f12d073cdbeb070806000ff273e12ff9923
Shubham Yadav <cshubyadav@gmail.com>
2025-04-29 19:48:51 +0530

    Proxy fix

--- trade-app/src/api.js
+++ trade-app/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 // Use relative paths ("/api/..."), proxy handles base URL
-const client = axios.create({ baseURL: '/api' });
+const API_URL = process.env.REACT_APP_API_URL ||
+                'https://tradelot-service.onrender.com/api';
+
+const client = axios.create({ baseURL: API_URL });
 client.defaults.headers.common['Content-Type'] = 'application/json';
 
 export const getAllTrades = () => client.get('/trades/getAllTrades');
